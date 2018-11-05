a9ce83cee8038bc2512b8b55a65f5d76ce0fb3e4
edcv131 <44343435+edcv131@users.noreply.github.com>
2018-11-04 16:32:06 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,57 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame()
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
 
- 
- client.on("guildMemberRemove", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-:rose: سيرفر جميل ومرتب بس مظلوم بالتفاعل :rose: 
 
-*:small_orange_diamond: السيرفر عادة جديد ووصل 102ومظلوم بالدعم ونبي نوصله 500 عشان نسوي جيف اواي علي كردتس وحسابات جميلة وشكراً.. :rose: :small_orange_diamond: *
-                               [ https://discord.gg/nm852f7 ] 
- :small_blue_diamond: الرابط : :small_blue_diamond: ... [ ${member}  ]
-**`)
-}).catch(console.error)
+client.on("ready", () => {
+  client.user.setPresence({
+    status: 'dnd',
+    game: { 
+       type: 1,
+       name: 'اسم البوت',
+       url: 'http://twitch.tv/Streammingg',
+      application_id: '262274898422923264',
+  
+    }
+      });
+  });
+  
 
-})
- 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-:rose: سيرفر جميل ومرتب بس مظلوم بالتفاعل :rose: 
-
-*:small_orange_diamond: السيرفر عادة جديد ووصل 102ومظلوم بالدعم ونبي نوصله 500 عشان نسوي جيف اواي علي كردتس وحسابات جميلة وشكراً.. :rose: :small_orange_diamond: *
-                               [ https://discord.gg/nm852f7 ] 
- :small_blue_diamond: الرابط : :small_blue_diamond: ... [ ${member}  ]
-**`)  
-}).catch(console.error)
-})
-client.login(process.env.BOT_TOKEN);
+  client.login("MjYyMjc0ODk4NDIyOTIzMjY0.DsEgOg.dnMV49VXfdc_OABiEmaX2Yz1jWI");
